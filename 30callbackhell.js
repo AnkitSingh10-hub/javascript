683e4ba0e806433d52490efb15728f420023f0ad
Ankit Singh <ankitsinghtheweeknd@gmail.com>
2025-04-13 02:52:05 +0545

    fx

--- 30callbackhell.js
+++ 30callbackhell.js
@@ -1,10 +1,11 @@
 function myFunction() {
     setTimeout(() => {
-        const data = { a: 1, b: 2, c: 3 }
-        console.log(data)
+
         setTimeout(function () {
             console.log("Hello World")
         }, 6000)
+        const data = { a: 1, b: 2, c: 3 }
+        console.log(data)
     }, 2000)
 }
 
